60276800bf0c346f539b51db39941214ad2a26b6
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 18:58:56 +0530

    ##

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import './index.css';
+import styles from './style';
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <div ></div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          NavABr
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Billing,
+          CardDeal,
+          Business,
+          Clients,
+          CTA,
+          Stats,
+          Footer,
+          Testimonials,
+          Hero,
+        </div>
+      </div>
     </div>
+
 );
 
 export default App;
