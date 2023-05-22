33bf7a8faa63ed2c6bc4e476afd8ca2424be7895
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 18:46:03 +0530

    $

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 
 
-const App = () => {
-  return (
+const App = () => (
     <div className="bg-primary w-full overflow-hidden">
       <h1>Hello</h1>
     </div>
-  );
-}
+);
 
 export default App;
